ff1db62d6b19374aef17d5c75b34b9476503b6cb
LuckyQu <super.lucky.qu@gmail.com>
2025-03-13 12:42:19 +0800

    添加注释

--- 01_HelloWorld/client/main.go
+++ 01_HelloWorld/client/main.go
@@ -9,16 +9,22 @@ import (
 )
 
 func main() {
+	//创建连接
 	conn, err := grpc.NewClient("localhost:7777", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	//代码执行完毕后及时关闭连接
 	defer conn.Close()
+	//处理错误
 	if err != nil {
 		panic(err)
 	}
+	//创建客户
 	client := hello_grpc.NewHelloGRPCClient(conn)
+	//调用方法
 	req, err := client.SayHi(context.Background(), &hello_grpc.Req{Message: "我需要一个浆果！"})
 	if err != nil {
 		panic(err)
 	}
+	//获取返回值
 	fmt.Println(req.GetMessage())
 
 }
--- 01_HelloWorld/server/main.go
+++ 01_HelloWorld/server/main.go
@@ -8,22 +8,27 @@ import (
 	"net"
 )
 
+// 取出server
 type server struct {
 	hello_grpc.UnimplementedHelloGRPCServer
 }
 
+// 挂载方法
 func (s *server) SayHi(ctx context.Context, req *hello_grpc.Req) (res *hello_grpc.Res, err error) {
 	fmt.Println(req.GetMessage())
 	return &hello_grpc.Res{Message: "浆果*1"}, nil
 }
 
 func main() {
+	//创建监听前置步骤
 	l, err := net.Listen("tcp", "localhost:7777")
 	if err != nil {
 		panic(err)
 	}
+	//注册服务
 	s := grpc.NewServer()
 	hello_grpc.RegisterHelloGRPCServer(s, &server{})
+	//创建监听
 	err = s.Serve(l)
 	if err != nil {
 		panic(err)
